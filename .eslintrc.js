3af432e449010583c333d569d7a3402ed482947f
jorge1g <jsgonzaga@uol.com.br>
2019-11-16 10:15:07 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
   env: {
     es6: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb-base', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: 'babel-eslint',
+
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-      'react',
-      'prettier'
-  ],
+  plugins: ['react', 'prettier'],
+  parser: 'babel-eslint',
   rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': [
-          'warn',
-          { extensions: ['.jsx', '.js']}
-      ],
-      'import/prefer-default-export': 'off'
+    'prettier/prettier': 'error',
+    'no-param-reassign': 'off',
+    'camel-case': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    camelcase: 'off',
+    'no-unused-vars': [
+      1,
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_|^next$',
+        varsIgnorePattern: '^React$',
+      },
+    ],
+    'class-methods-use-this': 'off',
   },
 };
